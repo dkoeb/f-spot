2554642181ea26dc623ee958d6a0e167c4055eb1
Daniel Köb <daniel.koeb@peony.at>
2016-04-27 20:28:00 +0200

    Clean up Metadata.

--- src/Core/FSpot.Utils/Metadata.cs
+++ src/Core/FSpot.Utils/Metadata.cs
@@ -28,99 +28,95 @@
 //
 
 using System;
-
-using Hyena;
-
-using TagLib;
-
 using GLib;
+using Hyena;
+using TagLib;
 
 namespace FSpot.Utils
 {
-    public static class Metadata
-    {
-        public static TagLib.Image.File Parse (SafeUri uri)
-        {
-            // Detect mime-type
-            string mime;
-            try {
-                var gfile = FileFactory.NewForUri (uri);
-                var info = gfile.QueryInfo ("standard::content-type", FileQueryInfoFlags.None, null);
-                mime = info.ContentType;
-            } catch (Exception e) {
-                Hyena.Log.DebugException (e);
-                return null;
-            }
+	public static class Metadata
+	{
+		public static TagLib.Image.File Parse (SafeUri uri)
+		{
+			// Detect mime-type
+			string mime;
+			try {
+				var gfile = FileFactory.NewForUri (uri);
+				var info = gfile.QueryInfo ("standard::content-type", FileQueryInfoFlags.None, null);
+				mime = info.ContentType;
+			} catch (Exception e) {
+				Hyena.Log.DebugException (e);
+				return null;
+			}
 
-            if (mime.StartsWith ("application/x-extension-")) {
-                // Works around broken metadata detection - https://bugzilla.gnome.org/show_bug.cgi?id=624781
-                mime = string.Format ($"taglib/{mime.Substring (24)}");
-            }
+			if (mime.StartsWith ("application/x-extension-", StringComparison.Ordinal)) {
+				// Works around broken metadata detection - https://bugzilla.gnome.org/show_bug.cgi?id=624781
+				mime = string.Format ($"taglib/{mime.Substring (24)}");
+			}
 
-            // Parse file
-            var res = new GIOTagLibFileAbstraction { Uri = uri };
-            var sidecar_uri = GetSidecarUri (uri);
-            var sidecar_res = new GIOTagLibFileAbstraction { Uri = sidecar_uri };
+			// Parse file
+			var res = new GIOTagLibFileAbstraction { Uri = uri };
+			var sidecarUri = GetSidecarUri (uri);
+			var sidecarRes = new GIOTagLibFileAbstraction { Uri = sidecarUri };
 
-            TagLib.Image.File file = null;
-            try {
-                file = TagLib.File.Create (res, mime, ReadStyle.Average) as TagLib.Image.File;
-            } catch (Exception) {
-                Hyena.Log.DebugFormat ($"Loading of metadata failed for file: {uri}, trying extension fallback");
-                
-                try {
-                    file = TagLib.File.Create (res, ReadStyle.Average) as TagLib.Image.File;
-                } catch (Exception e) {
-                    Hyena.Log.DebugFormat ($"Loading of metadata failed for file: {uri}");
-                    Hyena.Log.DebugException (e);
-                    return null;
-                }
-            }
+			TagLib.Image.File file;
+			try {
+				file = TagLib.File.Create (res, mime, ReadStyle.Average) as TagLib.Image.File;
+			} catch (Exception) {
+				Hyena.Log.DebugFormat ($"Loading of metadata failed for file: {uri}, trying extension fallback");
 
-            // Load XMP sidecar
-            var sidecar_file = GLib.FileFactory.NewForUri (sidecar_uri);
-            if (sidecar_file.Exists) {
-                file.ParseXmpSidecar (sidecar_res);
-            }
+				try {
+					file = TagLib.File.Create (res, ReadStyle.Average) as TagLib.Image.File;
+				} catch (Exception e) {
+					Hyena.Log.DebugFormat ($"Loading of metadata failed for file: {uri}");
+					Hyena.Log.DebugException (e);
+					return null;
+				}
+			}
 
-            return file;
-        }
+			// Load XMP sidecar
+			var sidecarFile = FileFactory.NewForUri (sidecarUri);
+			if (sidecarFile.Exists) {
+				file.ParseXmpSidecar (sidecarRes);
+			}
 
-        public static void SaveSafely (this TagLib.Image.File metadata, SafeUri photo_uri, bool always_sidecar)
-        {
-            if (always_sidecar || !metadata.Writeable || metadata.PossiblyCorrupt) {
-                if (!always_sidecar && metadata.PossiblyCorrupt) {
-					Hyena.Log.WarningFormat ($"Metadata of file {photo_uri} may be corrupt, refusing to write to it, falling back to XMP sidecar.");
-                }
+			return file;
+		}
 
-                var sidecar_res = new GIOTagLibFileAbstraction () { Uri = GetSidecarUri (photo_uri) };
+		public static void SaveSafely (this TagLib.Image.File metadata, SafeUri photoUri, bool alwaysSidecar)
+		{
+			if (alwaysSidecar || !metadata.Writeable || metadata.PossiblyCorrupt) {
+				if (!alwaysSidecar && metadata.PossiblyCorrupt) {
+					Hyena.Log.WarningFormat ($"Metadata of file {photoUri} may be corrupt, refusing to write to it, falling back to XMP sidecar.");
+				}
 
-                metadata.SaveXmpSidecar (sidecar_res);
-            } else {
-                metadata.Save ();
-            }
-        }
+				var sidecarRes = new GIOTagLibFileAbstraction { Uri = GetSidecarUri (photoUri) };
 
-        delegate SafeUri GenerateSideCarName (SafeUri photo_uri);
-        static readonly GenerateSideCarName[] SidecarNameGenerators = {
-            (p) => new SafeUri (p.AbsoluteUri + ".xmp"),
-            (p) => p.ReplaceExtension (".xmp"),
-        };
+				metadata.SaveXmpSidecar (sidecarRes);
+			} else {
+				metadata.Save ();
+			}
+		}
 
-        public static SafeUri GetSidecarUri (SafeUri photo_uri)
-        {
-            // First probe for existing sidecar files, use the one that's found.
-            foreach (var generator in SidecarNameGenerators) {
-                var name = generator (photo_uri);
-                var file = GLib.FileFactory.NewForUri (name);
-                if (file.Exists) {
-                    return name;
-                }
-            }
-            
+		delegate SafeUri GenerateSideCarName (SafeUri photoUri);
+		static readonly GenerateSideCarName[] SidecarNameGenerators = {
+			p => new SafeUri (p.AbsoluteUri + ".xmp"),
+			p => p.ReplaceExtension (".xmp"),
+		};
 
-            // Fall back to the default strategy.
-            return SidecarNameGenerators[0] (photo_uri);
-        }
-    }
+		static SafeUri GetSidecarUri (SafeUri photoUri)
+		{
+			// First probe for existing sidecar files, use the one that's found.
+			foreach (var generator in SidecarNameGenerators) {
+				var name = generator (photoUri);
+				var file = FileFactory.NewForUri (name);
+				if (file.Exists) {
+					return name;
+				}
+			}
+
+			// Fall back to the default strategy.
+			return SidecarNameGenerators[0] (photoUri);
+		}
+	}
 }
