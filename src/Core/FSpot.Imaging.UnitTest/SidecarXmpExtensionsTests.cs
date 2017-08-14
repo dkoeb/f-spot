ed987f5d63b531251582c5d1b8dba8790f2281a8
Daniel Köb <daniel.koeb@peony.at>
2017-08-14 09:00:53 +0200

    Move remaining tab-lib extensions to FSpot.Imaging.

--- src/Core/FSpot.Imaging.UnitTest/SidecarXmpExtensionsTests.cs
+++ src/Core/FSpot.Imaging.UnitTest/SidecarXmpExtensionsTests.cs
@@ -1,11 +1,13 @@
 //
-// MetadataTest.cs
+// SidecarXmpExtensionsTests.cs
 //
 // Author:
 //   Mike Gemünde <mike@gemuende.de>
+//   Ruben Vermeersch <ruben@savanne.be>
 //
 // Copyright (C) 2010 Novell, Inc.
 // Copyright (C) 2010 Mike Gemünde
+// Copyright (C) 2010 Ruben Vermeersch
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -27,15 +29,17 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-using FSpot.Utils.Tests;
+using System;
+using FSpot.Imaging;
+using Hyena;
 using NUnit.Framework;
 using TagLib;
 using TagLib.Xmp;
 
-namespace FSpot.Imaging.UnitTest
+namespace FSpot.Utils.Tests
 {
 	[TestFixture]
-	public class MetadataTest
+	public class SidecarXmpExtensionsTests
 	{
 		[SetUp]
 		public void Initialize ()
@@ -48,8 +52,9 @@ namespace FSpot.Imaging.UnitTest
 		{
 			// Tests the file in its original state
 			var uri = ImageTestHelper.CreateTempFile ("taglib-sample.jpg");
+			var res = new GIOTagLibFileAbstraction () { Uri = uri };
 
-			var file = MetadataService.Parse (uri);
+			var file = File.Create (res) as TagLib.Image.File;
 			Assert.IsNotNull (file);
 
 			XmpTag xmp = file.GetTag (TagTypes.XMP) as XmpTag;
@@ -63,7 +68,7 @@ namespace FSpot.Imaging.UnitTest
 				Assert.AreEqual (0, node.Children.Count);
 			}
 
-			Assert.AreEqual (new string [] { "Kirche Sulzbach" }, file.Keywords);
+			Assert.AreEqual (new string [] { "Kirche Sulzbach" }, file.ImageTag.Keywords);
 
 			ImageTestHelper.DeleteTempFile (uri);
 		}
@@ -73,10 +78,16 @@ namespace FSpot.Imaging.UnitTest
 		{
 			// Tests the file with a sidecar
 			var uri = ImageTestHelper.CreateTempFile ("taglib-sample.jpg");
+			var res = new GIOTagLibFileAbstraction () { Uri = uri };
 			var sidecar_uri = ImageTestHelper.CopySidecarToTest (uri, "taglib-sample.xmp");
+			var sidecar_res = new GIOTagLibFileAbstraction () { Uri = sidecar_uri };
 
-			var file = MetadataService.Parse (uri);
+			var file = File.Create (res) as TagLib.Image.File;
 			Assert.IsNotNull (file);
+
+			// Override by sidecar file
+			bool success = MetadataService.ParseXmpSidecar (file, sidecar_res);
+			Assert.IsTrue (success);
 
 			XmpTag xmp = file.GetTag (TagTypes.XMP) as XmpTag;
 			// Xmp.MicrosoftPhoto_1_.DateAcquired (XmpText/20) "2009-08-04T20:42:36Z"
@@ -86,7 +97,7 @@ namespace FSpot.Imaging.UnitTest
 				Assert.IsNull (node);
 			}
 
-			Assert.AreEqual (new string [] { "F-Spot", "metadata", "test" }, file.Keywords);
+			Assert.AreEqual (new string [] { "F-Spot", "metadata", "test" }, file.ImageTag.Keywords);
 
 			ImageTestHelper.DeleteTempFile (uri);
 			ImageTestHelper.DeleteTempFile (sidecar_uri);
@@ -97,10 +108,16 @@ namespace FSpot.Imaging.UnitTest
 		{
 			// Tests the file with a sidecar
 			var uri = ImageTestHelper.CreateTempFile ("taglib-sample.jpg");
+			var res = new GIOTagLibFileAbstraction () { Uri = uri };
 			var sidecar_uri = ImageTestHelper.CopySidecarToTest (uri, "taglib-sample-broken.xmp");
+			var sidecar_res = new GIOTagLibFileAbstraction () { Uri = sidecar_uri };
 
-			var file = MetadataService.Parse (uri);
+			var file = File.Create (res) as TagLib.Image.File;
 			Assert.IsNotNull (file);
+
+			// Override by sidecar file should fail
+			bool success = MetadataService.ParseXmpSidecar (file, sidecar_res);
+			Assert.IsFalse (success);
 
 			XmpTag xmp = file.GetTag (TagTypes.XMP) as XmpTag;
 			// Xmp.MicrosoftPhoto_1_.DateAcquired (XmpText/20) "2009-08-04T20:42:36Z"
@@ -112,21 +129,63 @@ namespace FSpot.Imaging.UnitTest
 				Assert.AreEqual (0, node.Children.Count);
 			}
 
-			Assert.AreEqual (new string [] { "Kirche Sulzbach" }, file.Keywords);
+			Assert.AreEqual (new string [] { "Kirche Sulzbach" }, file.ImageTag.Keywords);
 
 			ImageTestHelper.DeleteTempFile (uri);
 			ImageTestHelper.DeleteTempFile (sidecar_uri);
 		}
 
 		[Test]
-		public void ValidateWithBrokenMetadata ()
+		public void TestSidecarWrite ()
 		{
-			// Tests the file with a sidecar
-			var uri = ImageTestHelper.CreateTempFile ("taglib-sample-broken.jpg");
-			var sidecar_uri = ImageTestHelper.CopySidecarToTest (uri, "taglib-sample-broken.xmp");
+			var uri = ImageTestHelper.CreateTempFile ("taglib-sample.jpg");
+			var sidecar_uri = uri.ReplaceExtension (".xmp");
+			var res = new GIOTagLibFileAbstraction () { Uri = uri };
+			var sidecar_res = new GIOTagLibFileAbstraction () { Uri = sidecar_uri };
+			Assert.IsTrue (sidecar_uri.ToString ().EndsWith (".xmp"));
 
-			var file = MetadataService.Parse (uri);
-			Assert.IsNull (file);
+			var sidecar_file = GLib.FileFactory.NewForUri (sidecar_uri);
+			Assert.IsFalse (sidecar_file.Exists);
+
+			var file = File.Create (res) as TagLib.Image.File;
+			Assert.IsNotNull (file);
+
+			file.ImageTag.Keywords = new string [] { "Kirche Aarschot" };
+
+			// Validate writing of the sidecar
+			bool success = MetadataService.SaveXmpSidecar (file, sidecar_res);
+			Assert.IsTrue (success);
+			Assert.IsTrue (sidecar_file.Exists);
+
+			var target = "<x:xmpmeta xmlns:x=\"adobe:ns:meta/\"><rdf:RDF xm"
+				+ "lns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\">"
+				+ "<rdf:Description MicrosoftPhoto:DateAcquired=\"2009-08-0"
+				+ "4T20:42:36Z\" xmp:creatortool=\"Microsoft Windows Photo "
+				+ "Gallery 6.0.6001.18000\" tiff:software=\"Microsoft Windo"
+				+ "ws Photo Gallery 6.0.6001.18000\" tiff:Orientation=\"1\""
+				+ " MicrosoftPhoto:Rating=\"1\" xmp:Rating=\"1\" xmlns:tiff"
+				+ "=\"http://ns.adobe.com/tiff/1.0/\" xmlns:xmp=\"http://ns"
+				+ ".adobe.com/xap/1.0/\" xmlns:MicrosoftPhoto=\"http://ns.m"
+				+ "icrosoft.com/photo/1.0/\"><MicrosoftPhoto:LastKeywordXMP"
+				+ "><rdf:Bag><rdf:li>Kirche Sulzbach</rdf:li></rdf:Bag></Mi"
+				+ "crosoftPhoto:LastKeywordXMP><dc:subject xmlns:dc=\"http:"
+				+ "//purl.org/dc/elements/1.1/\"><rdf:Bag><rdf:li>Kirche Aa"
+				+ "rschot</rdf:li></rdf:Bag></dc:subject></rdf:Description>"
+				+ "</rdf:RDF></x:xmpmeta>";
+
+			string written;
+			var read_res = new GIOTagLibFileAbstraction () { Uri = sidecar_uri };
+			using (var stream = read_res.ReadStream) {
+				using (var reader = new System.IO.StreamReader (stream)) {
+					written = reader.ReadToEnd ();
+				}
+			}
+			Assert.AreEqual (target, written);
+
+			// Check that the file is unchanged
+			file = File.Create (res) as TagLib.Image.File;
+			var keywords = file.ImageTag.Keywords;
+			Assert.AreEqual (new string [] { "Kirche Sulzbach" }, keywords);
 
 			ImageTestHelper.DeleteTempFile (uri);
 			ImageTestHelper.DeleteTempFile (sidecar_uri);
